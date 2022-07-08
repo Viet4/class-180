04c419ccadef438a4e89c9cceb7dce954ac3b731
Viet4 <vietng912@gmail.com>
2022-07-07 20:53:33 -0400

    initial commit

---
+++ ar_navigation.js
@@ -0,0 +1,81 @@
+let coordinates = {}
+
+$(document).ready(function () {
+    get_coordinates();
+    render_elements();
+})
+
+function get_coordinates() {
+    let searchParams = new URLSearchParams(window.location.search)
+    if (searchParams.has('source') && searchParams.has('destination')) {
+        let source = searchParams.get('source')
+        let destination = searchParams.get('destination')
+        coordinates.source_lat = source.split(";")[0]
+        coordinates.source_lon = source.split(";")[1]
+        coordinates.destination_lat = destination.split(";")[0]
+        coordinates.destination_lon = destination.split(";")[1]
+    } else {
+        alert("Coordinates not selected!")
+        window.history.back();
+    }
+}
+
+function render_elements() {
+    $.ajax({
+        url:`https://api.mapbox.com/directions/v5/mapbox/driving/${coordinates.source_lon}%2C${coordinates.source_lat}%3B${coordinates.destination_lon}%2C${coordinates.destination_lat}?alternatives=true&geometries=polyline&steps=true&access_token=pk.eyJ1IjoiYXBvb3J2ZWxvdXMiLCJhIjoiY2ttZnlyMDgzMzlwNTJ4a240cmEzcG0xNyJ9.-nSyL0Gy2nifDibXJg4fTA`,
+        type:"get",
+        success: function(response){
+            let images = {
+                "turn_right":"ar_right.png",
+                "turn_left":"ar_right.png",
+                "slight_left":"ar_slight_left.png",
+                "slight_right":"ar_slight_right.png",
+                "start":"ar_start.png",
+                "turn_straight":"ar_straight.png",
+            }
+
+            let steps = response.routes[0].legs[0].steps;
+            for (let i = 0; i < steps.length; i++){
+                let image;
+                let distance = steps[i].distance;
+                let instruction = steps[i].maneuver.instruction;
+
+                if (instruction.includes("turn right")){
+                    image = "turn_right";
+                }
+                else if (instruction.includes("turn left")){
+                    image = "turn_left";
+                }
+                /*else if (instruction.includes("slight left")){
+                    image = "slight_left";
+                }
+                else if (instruction.includes("slight right")){
+                    image = "slight_right";
+                }
+                else if (instruction.includes("start")){
+                    image = "start";
+                }
+                else if (instruction.includes("turn straight")){
+                    image = "turn_straight";
+                }*/
+
+                if (i > 0){
+                    $("#scene-container").append(
+                        <a-entity
+                            gps-entity-place="latitude:${steps[i].maneuver.location[1]}; longitude:${steps[i].maneuver.location[0]};"
+                        >
+                            <a-image
+                                name="${instruction}"
+                                src="./assets/${images[image]}"
+                                look-at="#step_${i-1}"
+                                scale="5 5 5"
+                                id="step_${i}"
+                                position="0 0 0"
+                            ></a-image>
+                        </a-entity>
+                    );
+                }
+            }
+        }
+    })
+}
